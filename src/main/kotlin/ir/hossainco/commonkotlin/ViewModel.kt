33fd36c098b3a0808d7962932183996a1bff2cbc
Hossain Khademian <hco@post.com>
2017-12-27 00:24:46 +0330

    add new ViewModel utils

--- src/main/kotlin/ir/hossainco/commonkotlin/ViewModel.kt
+++ src/main/kotlin/ir/hossainco/commonkotlin/ViewModel.kt
@@ -5,6 +5,7 @@
 package ir.hossainco.commonkotlin
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.app.Application
 import android.arch.lifecycle.AndroidViewModel
 import android.arch.lifecycle.ViewModel
@@ -27,6 +28,20 @@ inline fun <VM : ViewModel, P> viewModelFactory(crossinline provider: SingleProv
 	}
 
 
+inline fun <reified VM : ViewModel> Fragment.viewModelProvider(crossinline provider: Provider<VM>)
+	= ViewModelProviders.of(this, viewModelFactory(provider)).get(VM::class.java)
+
+inline fun <reified VM : ViewModel> FragmentActivity.viewModelProvider(crossinline provider: Provider<VM>)
+	= ViewModelProviders.of(this, viewModelFactory(provider)).get(VM::class.java)
+
+
+inline fun <reified VM : ViewModel> Fragment.viewModelSingleProvider(crossinline provider: SingleProvider<VM, Fragment>)
+	= ViewModelProviders.of(this, viewModelFactory(provider, this)).get(VM::class.java)
+
+inline fun <reified VM : ViewModel> FragmentActivity.viewModelSingleProvider(crossinline provider: SingleProvider<VM, FragmentActivity>)
+	= ViewModelProviders.of(this, viewModelFactory(provider, this)).get(VM::class.java)
+
+
 inline fun <reified VM : ViewModel> Fragment.viewModelProviderLazy(mode: LazyThreadSafetyMode = LazyThreadSafetyMode.NONE, crossinline provider: Provider<VM>)
 	= lazy(mode) { ViewModelProviders.of(this, viewModelFactory(provider)).get(VM::class.java) }
 
@@ -41,36 +56,20 @@ inline fun <reified VM : ViewModel> FragmentActivity.viewModelSingleProviderLazy
 	= lazy(mode) { ViewModelProviders.of(this, viewModelFactory(provider, this)).get(VM::class.java) }
 
 
-inline fun <reified VM : ViewModel> Fragment.viewModelProvider(crossinline provider: Provider<VM>)
-	= ViewModelProviders.of(this, viewModelFactory(provider)).get(VM::class.java)
-
-inline fun <reified VM : ViewModel> FragmentActivity.viewModelProvider(crossinline provider: Provider<VM>)
-	= ViewModelProviders.of(this, viewModelFactory(provider)).get(VM::class.java)
-
-
-inline fun <reified VM : ViewModel> Fragment.viewModelSingleProvider(crossinline provider: SingleProvider<VM, Fragment>)
-	= ViewModelProviders.of(this, viewModelFactory(provider, this)).get(VM::class.java)
-
-inline fun <reified VM : ViewModel> FragmentActivity.viewModelSingleProvider(crossinline provider: SingleProvider<VM, FragmentActivity>)
-	= ViewModelProviders.of(this, viewModelFactory(provider, this)).get(VM::class.java)
-
 @SuppressLint("StaticFieldLeak")
 open class ApplicationViewModel<out APPLICATION : Application>
-(application: APPLICATION)
-	: AndroidViewModel(application) {
-	val app
-		get() = getApplication<APPLICATION>()
-
+(val app: APPLICATION)
+	: AndroidViewModel(app) {
 	val applicationContext
-		get() = getApplication<APPLICATION>()
+		get() = app
 
 	open val context: Context
-		get() = getApplication<APPLICATION>()
+		get() = app
 }
 
 @Suppress("UNCHECKED_CAST", "MemberVisibilityCanPrivate")
 @SuppressLint("StaticFieldLeak")
-open class ActivityViewModel<out APPLICATION : Application, out ACTIVITY : FragmentActivity>
+open class ActivityViewModel<out APPLICATION : Application, out ACTIVITY : Activity>
 (val activity: ACTIVITY)
 	: ApplicationViewModel<APPLICATION>(activity.application as APPLICATION) {
 	override val context: ACTIVITY
