05ed03b1fea702a662ede67e6edcaa46659c4036
Hossain Khademian <hco@post.com>
2017-12-27 11:02:02 +0330

    convert Observe<T,R> to SingleProvider<T,R>

--- src/main/kotlin/ir/hossainco/commonkotlin/android/livedata/Observe.kt
+++ src/main/kotlin/ir/hossainco/commonkotlin/android/livedata/Observe.kt
@@ -1,11 +1,12 @@
 package ir.hossainco.commonkotlin.android.livedata
 
 import android.arch.lifecycle.Observer
+import ir.hossainco.commonkotlin.provider.SingleProvider
 
-typealias Observe<T, R> = (T) -> R
+typealias Observe<T, R> = SingleProvider<T, R>
 
-fun <T> Observe<T, Any>.toObserverNotNull(): Observer<T> =
-	Observer { it!!.let(this) }
+fun <T> Observe<Any, T>.toObserverNotNull(): Observer<T>
+	= Observer { it!!.let(this) }
 
-fun <T> Observe<T, Any>.toObserver(): Observer<T> =
-	Observer { it?.let(this) }
+fun <T> Observe<Any, T>.toObserver(): Observer<T>
+	= Observer { it?.let(this) }
